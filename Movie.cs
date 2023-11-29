729cfd8d1beb12ac38f4fe70ff273cf80707e293
jlawall9 <74278476+jlawall9@users.noreply.github.com>
2023-11-28 20:57:44 -0500

    Began Movie and Show class

---
+++ Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS3309_TheatreGroupProject
+{
+    class Movie
+    {
+        public string Title;
+        public enum Genre { Horror, Thriller, History, Action, Romance, Comedy}
+        public Genre genre;
+        public string Img;
+        public string rating;
+        public string Director;
+        public string description;
+    }
+}
---
+++ Show.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS3309_TheatreGroupProject
+{
+    class Show
+    {
+        //This object will hold seat availibility, theater room and movie
+    }
+}
