986d502cb61142c6e025ea2de999cc14ceeb63ba
jlawall9 <74278476+jlawall9@users.noreply.github.com>
2023-12-01 13:38:58 -0500

    Added SQL statments for movie

---
+++ MovieManageSystem.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CIS3309_TheatreGroupProject
+{
+    class MovieManageSystem
+    {
+        public static OleDbConnection myConnection = new OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data Source=Movies.accdb");
+
+
+        public static DataTable LoadMoiveDB()
+        {
+            
+            OleDbDataAdapter myDataAdapter;
+            DataSet movieDataSet;
+            DataTable movieTable;
+           
+            string strSQL;
+
+
+            strSQL = "SELECT * FROM MoviesTable";
+            myDataAdapter = new OleDbDataAdapter(strSQL, myConnection);
+            movieDataSet = new DataSet("MoviesTable");
+            myDataAdapter.Fill(movieDataSet, "MovieTable");
+
+            movieTable = movieDataSet.Tables["MovieTable"];
+            string joe = "hey";
+            return movieTable;
+
+        }
+   
+        /// /////////////////////////////////////////  INSERT  ///////////////////////////////////////////////////////////////
+        public static void InsertMovieDB(Movie movie)
+        {
+            string ih = "hi";
+            // SQL STATMENT
+            string strSQL = "INSERT INTO MoviesTable (Title, Genre, Director, Rating, Description, Img)" +
+            " VALUES ('" + movie.Title + "','" + movie.MovieGenre + "','" + movie.Director + "','" + movie.Rating + "','" +
+            movie.Description + "','" + movie.Img + "');";
+
+            OleDbCommand insertCommand = new OleDbCommand(strSQL, myConnection);
+
+            try
+            {
+                myConnection.Open();
+                insertCommand.ExecuteNonQuery();
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+
+        }
+
+
+    
+
+
+
+        /// //////////////////////////////////////////////////////  DELETE  ///////////////////////////////////////
+
+
+        public static void DeleteMovieDB(string name)
+        {
+            string strSQL = "DELETE FROM MovieTable WHERE Title ='" + name + "'";
+            OleDbCommand deleteCommand = new OleDbCommand(strSQL, myConnection);
+            try
+            {
+                myConnection.Open();
+                deleteCommand.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+
+        }
+    }
+}
---
+++ ShowManageSystem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS3309_TheatreGroupProject
+{
+    class ShowManageSystem
+    {
+        public static OleDbConnection myConnection = new OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data Source=Shows.accdb");
+
+        public static DataTable LoadShowDB()
+        {
+            return null;
+
+        }
+       
+
+        /// /////////////////////////////////////////  INSERT  ///////////////////////////////////////////////////////////////
+        public static void InsertShowDB(Show show)
+        {
+
+            /*
+            // SQL STATMENT
+            string strSQL = "INSERT INTO MoviesTable (Title, Genre, Director, Rating, Description, Img)" +
+            " VALUES ('" + movie.Title + "','" + movie.MovieGenre + "','" + movie.Director + "','" + movie.Rating + "','" +
+            movie.Description + "','" + movie.Img + "');";
+
+            OleDbCommand insertCommand = new OleDbCommand(strSQL, myConnection);
+
+            try
+            {
+                myConnection.Open();
+                insertCommand.ExecuteNonQuery();
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            */
+        }
+
+
+
+
+        /// //////////////////////////////////////////////////////  DELETE  ///////////////////////////////////////
+
+
+        public static void DeleteShowDB(string name)
+        {
+            /*string strSQL = "DELETE FROM MovieTable WHERE Title ='" + name + "'";
+            OleDbCommand deleteCommand = new OleDbCommand(strSQL, myConnection);
+            try
+            {
+                myConnection.Open();
+                deleteCommand.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            */
+        }
+    }
+}
+    
